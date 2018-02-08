28139870dfc820da7770f35de35eff2ca1994bb6
Blake Barrett <blake@blakebarrett.net>
2018-02-07 23:30:39 -0800

    Refresh and Filter, both function as expected.

--- app/src/main/java/com/blakebarrett/tubitest/MainActivity.java
+++ app/src/main/java/com/blakebarrett/tubitest/MainActivity.java
@@ -2,14 +2,23 @@ package com.blakebarrett.tubitest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.blakebarrett.tubitest.api.MovieManager;
 import com.blakebarrett.tubitest.controllers.MovieAdapter;
+import com.blakebarrett.tubitest.models.IMovie;
+
+import java.util.Comparator;
 
 public class MainActivity extends Activity {
 
     private ListView listView;
+    private Button refreshButton;
+    private Button filterButton;
+
     private MovieAdapter adapter;
 
     @Override
@@ -17,7 +26,33 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.listView = findViewById(R.id.movies_list_view);
+        this.refreshButton = findViewById(R.id.refreshButton);
+        this.filterButton = findViewById(R.id.filterButton);
+
         fetchMovies();
+
+        this.refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                listView.setVisibility(View.GONE);
+                fetchMovies();
+            }
+        });
+
+        this.filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                adapter.getMovies().sort(new Comparator<IMovie>() {
+                    @Override
+                    public int compare(final IMovie first, final IMovie second) {
+                        final int id1 = Integer.parseInt(first.getId());
+                        final int id2 = Integer.parseInt(second.getId());
+                        return id1 - id2;
+                    }
+                });
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void fetchMovies() {
@@ -27,6 +62,7 @@ public class MainActivity extends Activity {
             public void run() {
                 adapter = new MovieAdapter(MainActivity.this, this.movies);
                 listView.setAdapter(adapter);
+                listView.setVisibility(View.VISIBLE);
             }
         });
     }
